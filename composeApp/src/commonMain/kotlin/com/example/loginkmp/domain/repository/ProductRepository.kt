be7c349c96db537b8963063744163772b35011b9
Cizer Thapa <cizerthapa@gmail.com>
2026-01-13 17:06:58 +0545

    chore: auto indent added for the work file

--- composeApp/src/commonMain/kotlin/com/example/loginkmp/domain/repository/ProductRepository.kt
+++ composeApp/src/commonMain/kotlin/com/example/loginkmp/domain/repository/ProductRepository.kt
@@ -6,8 +6,6 @@ import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
     val products: Flow<List<Product>>
-    
-
     suspend fun getProducts(page: Int = 1, limit: Int = 20): Result<ProductsResponse>
     suspend fun searchProducts(query: String): Result<ProductsResponse>
     suspend fun getProduct(id: Int): Result<Product>
